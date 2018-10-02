58dabfe00a0267f92bfeb7c7cdab22aa802dfa57
zarela zanabria <zarelixzana@gmail.com>
2018-10-02 01:52:20 -0500

    Mostramos las Tareas guardadas

--- src/app/components/tarea/tarea-list/tarea-list.component.ts
+++ src/app/components/tarea/tarea-list/tarea-list.component.ts
@@ -5,6 +5,8 @@ import { TareasService } from '../../../services/tareas.service';
 
 //Tareas Class
 import { Tareas } from '../../../models/tareas';
+// toastr
+/* import { ToastrService } from 'ngx-toastr'; */
 
 @Component({
   selector: 'app-tarea-list',
@@ -12,11 +14,16 @@ import { Tareas } from '../../../models/tareas';
   styleUrls: ['./tarea-list.component.css']
 })
 export class TareaListComponent implements OnInit {
+  //Creamos un Arreglo para poder manipular los datos ya tenemos un arreglo de Tareas
 
   tareaList: Tareas[];
   /*Comectarnos con el Servicio en donde estan los métodos 
   Nos traemos el servicio y la importamos en el constructor*/
-  constructor(private tareaService: TareasService) { }
+  constructor(
+    private tareaService: TareasService,
+    
+    )
+     { }
 
   /**Necesitamos mostrar los datos de Firebase y para ello vamos a ejecutar
     ocurren dentro de la base e datos
@@ -24,10 +31,27 @@ export class TareaListComponent implements OnInit {
   ngOnInit() {
     /* Ejecutamos el metodo que nos permite traer todos los datos de Firebase
     Para ello utilizamos snapshotChanges que nos trae todo los cambios que 
-    ocurren dentro de la base e datos */
-    this.tareaService.getTareas()
+    ocurren dentro de la base e datos 
+    subscribe escuchando todo los cambios nos trae los propios datos que guardamos 
+    */
+    return this.tareaService.getTareas()
       .snapshotChanges()
-      .subscribe(item =>)
+       .subscribe(item =>{
+        //Lo inicializamos en blanco
+        this.tareaList=[];
+        //Recorrer cada item para poder modificar
+        item.forEach( element=>{
+          //El contemido del element convertimos al Jsony los almacenados en tar
+          let tar = element.payload.toJSON();
+          tar['$key']=element.key;
+          //Asignamos a la lista el elemento tar que es una Tarea
+          this.tareaList.push(tar as Tareas);
+        });
+      });
+  }
+
+  onEdit (tarea : Tareas){
+    this.tareaService.selectedTarea = Object.assign({},tarea);
 
   }
 
